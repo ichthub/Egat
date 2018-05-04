539558aa61e9229be3a556429edd82219bc797eb
Hassan <hassanhoumaid92@gmail.com>
2018-05-04 11:02:25 +0100

    test heroku server update

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({hi: "hassan"});
+  res.send({Hello: "TSDI members"});
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// https://shrouded-fjord-57164.herokuapp.com/ | https://git.heroku.com/shrouded-fjord-57164.git
