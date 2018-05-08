78f12e9ec10f5a0fc8d11320e9b35d79f1754b24
Hassan <hassanhoumaid92@gmail.com>
2018-05-08 19:05:14 +0100

    test_2

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,12 @@
 // if in production - return the prod set of keys
 // if in developement - return the dev set of keys
-const keys =
+/* const keys =
   process.env.NODE_ENV === "production"
     ? require("./prodkeys")
     : require("./devKeys");
+*/
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prodkeys");
+} else {
+  module.exports = require("./devKeys");
+}
